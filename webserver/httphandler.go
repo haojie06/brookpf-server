7ac4be30be5c2f836927179db3b11faf526cd2cc
haojie06 <haojie06@qq.com>
2020-08-13 08:34:50 +0800

    受控端跨域问题

--- webserver/httphandler.go
+++ webserver/httphandler.go
@@ -150,10 +150,12 @@ func editServer(w http.ResponseWriter, r *http.Request) {
 
 //登录验证以及授权
 func auth(w http.ResponseWriter, r *http.Request) bool {
+	indexHandler(w, r)
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return false
 	}
+
 	uname := r.PostFormValue("Username")
 	pword := r.PostFormValue("Password")
 	if uname != username || pword != password {
@@ -168,4 +170,20 @@ func auth(w http.ResponseWriter, r *http.Request) bool {
 		//授权成功
 		return true
 	}
+}
+
+//跨域请求
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	setupResponse(&w, req)
+	if (*req).Method == "OPTIONS" {
+		return
+	}
+
+	// process the request...
 }
