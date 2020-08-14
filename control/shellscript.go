6f70a73d3db4a4df6bc19d60703c512eeb85e527
haojie06 <haojie06@qq.com>
2020-08-14 22:42:01 +0800

    添加流量记录前先删除原来的

--- control/shellscript.go
+++ control/shellscript.go
@@ -26,6 +26,10 @@ func changeIptables(add bool, port string) {
 	//修改iptables
 	log.Println("[iptables修改]修改iptables")
 	if add {
+		log.Printf("[iptables修改]%s\n", string(executeCommand("iptables -D INPUT -p tcp --dport "+port)))
+		log.Printf("[iptables修改]%s\n", string(executeCommand("iptables -D INPUT -p udp --dport "+port)))
+		log.Printf("[iptables修改]%s\n", string(executeCommand("iptables -D OUTPUT -p tcp --sport "+port)))
+		log.Printf("[iptables修改]%s\n", string(executeCommand("iptables -D OUTPUT -p udp --sport "+port)))
 		log.Printf("[iptables修改]添加TCP:\n%s\n", string(executeCommand("iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport "+port+" -j ACCEPT")))
 		log.Printf("[iptables修改]添加UDP:\n%s\n", string(executeCommand("iptables -I INPUT -m state --state NEW -m udp -p udp --dport "+port+" -j ACCEPT")))
 		log.Printf("[iptables修改]%s\n", string(executeCommand("iptables -I INPUT -p tcp --dport "+port)))
